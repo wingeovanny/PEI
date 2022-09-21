647ab281e76f60010a0b0e003fc82bbdc1499dc4
geovanny <wingeovanny>
2022-09-21 01:47:18 -0500

    implementacion de JWT

--- src/main/java/com/challenge/Application.java
+++ src/main/java/com/challenge/Application.java
@@ -2,7 +2,15 @@ package com.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.challenge.seguridad.JWTAuthorizationFilter;
 
 @SpringBootApplication
 @EnableAsync
@@ -10,4 +18,19 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @EnableWebSecurity
+    @Configuration
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/user").permitAll()
+                    .anyRequest().authenticated();
+        }
+    }
+
 }
